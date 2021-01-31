03670dcd82f159b32ce114c520008802e12894ba
dominikg <dominik.goepel@gmx.de>
2021-01-31 19:28:36 +0100

    wip: remove vite related things; copy rollup-plugin-svelte-hot; update to latest vite

--- packages/vite-plugin-svelte/src/index.ts
+++ packages/vite-plugin-svelte/src/index.ts
@@ -1,18 +1,35 @@
-// import fs from 'fs'
-import { Plugin, ViteDevServer } from 'vite'
-import svelte from 'rollup-plugin-svelte'
-// import { parseVueRequest } from './utils/query'
-// import { getDescriptor } from './utils/descriptorCache'
-// import { getResolvedScript } from './script'
-// import { transformMain } from './main'
-// import { handleHotUpdate } from './handleHotUpdate'
-// import { transformTemplateAsModule } from './template'
-// import { transformStyle } from './style'
+import * as path from 'path'
+import { HmrContext, ModuleNode, Plugin, ViteDevServer } from 'vite'
+import { createFilter } from '@rollup/pluginutils'
+// @ts-ignore
+import * as relative from 'require-relative'
+import { compile, preprocess, walk } from 'svelte/compiler'
+// @ts-ignore
+import { createMakeHot } from 'svelte-hmr'
 
-export { parseVueRequest, VueQuery } from './utils/query'
+const PREFIX = '[rollup-plugin-svelte]'
+const pkg_export_errors = new Set()
+
+const plugin_options = new Set([
+  'emitCss',
+  'exclude',
+  'extensions',
+  'include',
+  'onwarn',
+  'preprocess',
+  'hot'
+])
 
 export interface Options {
   // TODO extend rollup plugin svelte options
+  emitCss: boolean
+  compilerOptions: any
+  preprocess: any
+  extensions: any
+  include: any
+  exclude: any
+  onwarn: any
+  hot: any
 }
 
 export interface ResolvedOptions extends Options {
@@ -22,19 +39,47 @@ export interface ResolvedOptions extends Options {
 }
 
 export default function vitePluginSvelte(rawOptions: Options): Plugin {
+  const { compilerOptions = {}, ...rest } = rawOptions
+  const extensions = rest.extensions || ['.svelte']
+  const filter = createFilter(rest.include, rest.exclude)
+
+  compilerOptions.format = 'esm'
+
+  for (const key in rest) {
+    if (plugin_options.has(key)) continue
+    console.warn(
+      `${PREFIX} Unknown "${key}" option. Please use "compilerOptions" for any Svelte compiler configuration.`
+    )
+  }
+
+  // [filename]:[chunk]
+  const cache_emit = new Map()
+  const { onwarn, emitCss = true } = rest
+
+  if (emitCss) {
+    if (compilerOptions.css) {
+      console.warn(
+        `${PREFIX} Forcing \`"compilerOptions.css": false\` because "emitCss" was truthy.`
+      )
+    }
+    compilerOptions.css = false
+  }
+
+  if (rest.hot && !compilerOptions.dev) {
+    console.info(`${PREFIX} Disabling HMR because "dev" option is disabled.`)
+    rest.hot = false
+  }
+
+  const makeHot = rest.hot && createMakeHot({ walk })
+
   let options: ResolvedOptions = {
     isProduction: process.env.NODE_ENV === 'production',
     ...rawOptions,
     root: process.cwd()
   }
-  const rollupPluginSvelte = svelte(options as any)
-  if (rollupPluginSvelte == null) {
-    throw new Error('failed to init')
-  }
 
   return {
-    ...rollupPluginSvelte,
-    name: 'vite:svelte',
+    name: 'vite-plugin-svelte',
 
     config(config) {
       // patch vite config
@@ -60,18 +105,114 @@ export default function vitePluginSvelte(rawOptions: Options): Plugin {
     load(id, ssr) {
       // we should properbly handle stripping the query params here, see vite vue plugin
       // not sure if we also need to do sth different during ssr?
+
       // @ts-ignore
-      return rollupPluginSvelte.load(id)
+      const result = cache_emit.get(id) || null
+      console.log('load', { id, result })
+      return result
     },
+
     async resolveId(importee, importer, options, ssr) {
-      // see load, maybe we need to check ssr flag here
-      // @ts-ignore
-      return rollupPluginSvelte?.resolveId(importee, importer, options)
+      if (cache_emit.has(importee)) return importee
+      if (
+        !importer ||
+        importee[0] === '.' ||
+        importee[0] === '\0' ||
+        path.isAbsolute(importee)
+      )
+        return null
+
+      // if this is a bare import, see if there's a valid pkg.svelte
+      const parts = importee.split('/')
+
+      let dir,
+        pkg,
+        name = parts.shift()
+      if (name && name[0] === '@') {
+        name += `/${parts.shift()}`
+      }
+
+      try {
+        const file = `${name}/package.json`
+        const resolved = relative.resolve(file, path.dirname(importer))
+        dir = path.dirname(resolved)
+        pkg = require(resolved)
+      } catch (err) {
+        if (err.code === 'MODULE_NOT_FOUND') return null
+        if (err.code === 'ERR_PACKAGE_PATH_NOT_EXPORTED') {
+          pkg_export_errors.add(name)
+          return null
+        }
+        throw err
+      }
+
+      // use pkg.svelte
+      if (parts.length === 0 && pkg.svelte) {
+        return path.resolve(dir, pkg.svelte)
+      }
     },
+
     async transform(code, id, ssr) {
-      // this could be tricky, we need to pass ssr flag to svelte compiler inside
-      // @ts-ignore
-      return rollupPluginSvelte?.transform(code, id)
+      if (!filter(id)) return null
+
+      const extension = path.extname(id)
+      if (!~extensions.indexOf(extension)) return null
+
+      const dependencies = []
+      const filename = path.relative(process.cwd(), id)
+      const svelte_options = { ...compilerOptions, filename }
+
+      if (rest.preprocess) {
+        const processed = await preprocess(code, rest.preprocess, { filename })
+        if (processed.dependencies) dependencies.push(...processed.dependencies)
+        if (processed.map) svelte_options.sourcemap = processed.map
+        code = processed.code
+      }
+
+      const compiled = compile(code, svelte_options)
+
+      ;(compiled.warnings || []).forEach((warning) => {
+        if (!emitCss && warning.code === 'css-unused-selector') return
+        if (onwarn) onwarn(warning, this.warn)
+        else this.warn(warning)
+      })
+
+      if (emitCss && compiled.css.code) {
+        const fname = id.replace(new RegExp(`\\${extension}$`), '.css')
+        compiled.js.code += `\nimport ${JSON.stringify(fname)};\n`
+        cache_emit.set(fname, compiled.css)
+      }
+
+      if (makeHot) {
+        compiled.js.code = makeHot({
+          id,
+          compiledCode: compiled.js.code,
+          hotOptions: {
+            injectCss: !rest.emitCss,
+            ...rest.hot
+          },
+          compiled,
+          originalCode: code,
+          compileOptions: compilerOptions
+        })
+      }
+
+      if (this.addWatchFile) {
+        dependencies.forEach(this.addWatchFile)
+      } else {
+        compiled.js.dependencies = dependencies
+      }
+
+      const result = compiled.js
+
+      console.log('transform', { id, result })
+      return result
+    },
+
+    handleHotUpdate(
+      ctx: HmrContext
+    ): Array<ModuleNode> | void | Promise<Array<ModuleNode> | void> {
+      console.log('handleHotUpdate', ctx)
     }
 
     /*
--- packages/vite-plugin-svelte/src/utils/query.ts
+++ packages/vite-plugin-svelte/src/utils/query.ts
@@ -1,18 +1,18 @@
 import qs from 'querystring'
 
-export interface VueQuery {
-  vue?: boolean
+export interface SvelteQuery {
+  svelte?: boolean
   src?: boolean
   type?: 'script' | 'template' | 'style' | 'custom'
   index?: number
   lang?: string
 }
 
-export function parseVueRequest(id: string) {
+export function parseSvelteRequest(id: string) {
   const [filename, rawQuery] = id.split(`?`, 2)
-  const query = qs.parse(rawQuery) as VueQuery
-  if (query.vue != null) {
-    query.vue = true
+  const query = qs.parse(rawQuery) as SvelteQuery
+  if (query.svelte != null) {
+    query.svelte = true
   }
   if (query.src != null) {
     query.src = true
