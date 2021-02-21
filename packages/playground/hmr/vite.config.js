5cd7c122e8b2db386cacf057d5f2e8b94579afae
dominikg <dominik.goepel@gmx.de>
2021-02-21 16:32:58 +0100

    wip: add debug code to find resolve issue on windows ci tests

--- packages/playground/hmr/vite.config.js
+++ packages/playground/hmr/vite.config.js
@@ -1,5 +1,26 @@
-const svelte = require('@svitejs/vite-plugin-svelte')
 const { defineConfig } = require('vite')
+const fs = require('fs')
+
+let svelte
+
+try {
+  svelte = require('@svitejs/vite-plugin-svelte')
+} catch (e) {
+  let path, exists
+  try {
+    path = require.resolve('@svitejs/vite-plugin-svelte')
+    exists = fs.existsSync(path)
+    console.log("require  resolve '@svitejs/vite-plugin-svelte'", {
+      path,
+      exists
+    })
+  } catch (e1) {
+    console.error('require resolve failed', e1)
+  }
+
+  console.error('failed ro require', e)
+  throw e
+}
 
 module.exports = defineConfig(({ command, mode }) => {
   const isProduction = mode === 'production'
