8c99cc3d20f783d612dddf115a8d5fac3ffbdd74
dominikg <dominik.goepel@gmx.de>
2021-03-24 19:47:22 +0100

    wip: update deps

--- pnpmfile.js
+++ pnpmfile.js
@@ -6,10 +6,7 @@ function readPackage(pkg) {
 		'optionalDependencies'
 	]) {
 		// enforce use of workspace vite-plugin-svelte
-		// TODO remove linking of svitejs one once kit released migration
-		if (pkg[section]['@svitejs/vite-plugin-svelte']) {
-			pkg[section]['@svitejs/vite-plugin-svelte'] = 'workspace:@sveltejs/vite-plugin-svelte@*';
-		} else if (pkg[section]['@sveltejs/vite-plugin-svelte']) {
+		if (pkg[section]['@sveltejs/vite-plugin-svelte']) {
 			pkg[section]['@sveltejs/vite-plugin-svelte'] = 'workspace:*';
 		}
 	}
