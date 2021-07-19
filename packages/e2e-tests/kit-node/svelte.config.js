7287a9d35655f85f99d17a1110cd168a5b4579f8
dominikg <dominik.goepel@gmx.de>
2021-07-19 15:34:08 +0200

    fix: adapter-node failed with empty options, remove

--- packages/e2e-tests/kit-node/svelte.config.js
+++ packages/e2e-tests/kit-node/svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: node({}),
+		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
