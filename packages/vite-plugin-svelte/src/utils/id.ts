ed2a22d0e411b71d39311a5026b3c72762e90e4b
Bjorn Lu <34116392+bluwy@users.noreply.github.com>
2021-07-21 17:17:29 +0800

    chore: replace querystring with URLSearchParams (#107)
    * chore: replace querystring with URLSearchParams
    
    * refactor: improve query parser

--- packages/vite-plugin-svelte/src/utils/id.ts
+++ packages/vite-plugin-svelte/src/utils/id.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-import qs from 'querystring';
 import { createFilter } from '@rollup/pluginutils';
 import { Arrayable, ResolvedOptions } from './options';
 import { normalizePath } from 'vite';
@@ -44,13 +43,7 @@ function parseToSvelteRequest(
 	timestamp: number,
 	ssr: boolean
 ): SvelteRequest | undefined {
-	const query: RequestQuery = qs.parse(rawQuery) as RequestQuery;
-	for (const p of ['svelte', 'url', 'raw'] as Array<keyof RequestQuery>) {
-		if (query[p] != null) {
-			// @ts-ignore
-			query[p] = true;
-		}
-	}
+	const query = parseRequestQuery(rawQuery);
 	if (query.url || query.raw) {
 		// skip requests with special vite tags
 		return;
@@ -83,6 +76,17 @@ function createVirtualImportId(filename: string, root: string, type: SvelteQuery
 	}
 	// return same virtual id format as vite-plugin-vue eg ...App.svelte?svelte&type=style&lang.css
 	return `${filename}?${parts.join('&')}`;
+}
+
+function parseRequestQuery(rawQuery: string): RequestQuery {
+	const query = Object.fromEntries(new URLSearchParams(rawQuery));
+	for (const key in query) {
+		if (query[key] === '') {
+			// @ts-ignore
+			query[key] = true;
+		}
+	}
+	return query as RequestQuery;
 }
 
 /**
