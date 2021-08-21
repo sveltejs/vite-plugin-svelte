a6236dc8095a734353c7210ff1c0e509ee726385
Dominik G <dominik.goepel@gmx.de>
2021-08-21 19:21:30 +0200

    docs: fix changelog codeblock and prevent eslint from interfering (#150)

--- .eslintrc.js
+++ .eslintrc.js
@@ -104,6 +104,7 @@ module.exports = {
 			rules: {
 				'no-undef': 'off',
 				'no-unused-vars': 'off',
+				'no-unused-labels': 'off',
 				'no-console': 'off',
 				'padded-blocks': 'off',
 				'node/no-missing-import': 'off',
@@ -117,6 +118,7 @@ module.exports = {
 			rules: {
 				'no-undef': 'off',
 				'no-unused-vars': 'off',
+				'no-unused-labels': 'off',
 				'no-console': 'off',
 				'padded-blocks': 'off',
 				'node/no-missing-import': 'off',
