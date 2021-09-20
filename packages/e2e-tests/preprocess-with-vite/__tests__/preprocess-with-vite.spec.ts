31653ad8e0ca5608e78ffa8f86e8913ab1ffb65b
bluwy <bjornlu.dev@gmail.com>
2021-09-21 02:21:19 +0800

    fix: typo

--- packages/e2e-tests/preprocess-with-vite/__tests__/preprocess-with-vite.spec.ts
+++ packages/e2e-tests/preprocess-with-vite/__tests__/preprocess-with-vite.spec.ts
@@ -1,7 +1,7 @@
 import { getColor, getText } from 'testUtils';
 
 test('should render App', async () => {
-	expect(await getText('h1.foo')).toBe(`Hello World`);
+	expect(await getText('h1.foo')).toBe(`Hello world`);
 	expect(await getColor('#app-scss')).toBe('#000099'); // darken($blue, 20)
 	expect(await getText('#foo-title')).toBe(`Styles with stylus blub`);
 	expect(await getColor('#foo-title')).toBe('magenta');
