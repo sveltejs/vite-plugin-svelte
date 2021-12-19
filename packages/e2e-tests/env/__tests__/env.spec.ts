0d0b6cbaa7836b258f51992623e65c30cdfab0e3
bluwy <bjornlu.dev@gmail.com>
2021-12-19 15:33:31 +0800

    fix: test

--- packages/e2e-tests/env/__tests__/env.spec.ts
+++ packages/e2e-tests/env/__tests__/env.spec.ts
@@ -1,5 +1,8 @@
 import { findAssetFile, isBuild } from 'testUtils';
 
+// can't have no tests for test:serve
+it('dummy', () => {});
+
 if (isBuild) {
 	it('custom production mode should build for production', () => {
 		const indexBundle = findAssetFile(/index\..*\.js/);
