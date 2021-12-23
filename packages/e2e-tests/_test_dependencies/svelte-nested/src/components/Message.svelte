d40697accda1911c8f11830aba49a0d31b3c5f3d
Bjorn Lu <bjornlu.dev@gmail.com>
2021-12-23 22:37:21 +0800

    fix: skip prebundle non-js nested dependencies (#234)

--- packages/e2e-tests/_test_dependencies/svelte-nested/src/components/Message.svelte
+++ packages/e2e-tests/_test_dependencies/svelte-nested/src/components/Message.svelte
@@ -2,6 +2,7 @@
 	export let id = 'id';
 	export let message = '';
 	import { cjs_and_esm } from 'e2e-test-dep-cjs-and-esm';
+	import 'e2e-test-dep-scss-only';
 </script>
 
 <div {id}>{message}</div>
