9c682e97b3448f4afa11b2d471131f99326356bf
bluwy <bjornlu.dev@gmail.com>
2022-02-27 01:37:02 +0800

    chore: fix vite plugins

--- packages/e2e-tests/_test_dependencies/vite-plugins/index.js
+++ packages/e2e-tests/_test_dependencies/vite-plugins/index.js
@@ -19,9 +19,9 @@ function transformValidation() {
 			name: 'transform-validation:2',
 			transform(code, id) {
 				if (id.endsWith('.svelte')) {
-					return code.replace('__JS_TRANSFORM_2__', '__JS_TRANSFORM_3__');
+					return code.replace('__JS_TRANSFORM_2__', 'Hello world');
 				} else if (id.endsWith('.css')) {
-					return code.replace('__CSS_TRANSFORM_2__', '__CSS_TRANSFORM_3__');
+					return code.replace('__CSS_TRANSFORM_2__', 'red');
 				}
 			}
 		},
@@ -31,9 +31,8 @@ function transformValidation() {
 			transform(code, id) {
 				if (id.endsWith('.svelte')) {
 					return code.replace('__JS_TRANSFORM_3__', 'Hello world');
-				} else if (id.endsWith('.css')) {
-					return code.replace('__CSS_TRANSFORM_3__', 'red');
 				}
+				// can't handle css here as in build, it would be `export default {}`
 			}
 		}
 	];
