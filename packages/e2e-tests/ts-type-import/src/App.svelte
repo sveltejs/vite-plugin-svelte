6f22f9cc38845b4e1229101dc31f4b32c9b1230c
bluwy <bjornlu.dev@gmail.com>
2022-05-06 16:06:06 +0800

    fix: test

--- packages/e2e-tests/ts-type-import/src/App.svelte
+++ packages/e2e-tests/ts-type-import/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { world } from './world.js';
+	import { foobar } from './foobar.js';
 	let s: string = 'world';
 </script>
 
 <div id="hello">Hello {s}</div>
-<div id="world">{world()}</div>
+<div id="foobar">{foobar()}</div>
