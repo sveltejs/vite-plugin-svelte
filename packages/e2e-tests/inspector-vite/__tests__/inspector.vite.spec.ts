9ff187e6e7389427148b71cb7ccb52de1834548c
dominikg <dominik.goepel@gmx.de>
2022-05-10 22:47:09 +0200

    test: add tests for inspector with kit and vite templates

---
+++ packages/e2e-tests/inspector-vite/__tests__/inspector.vite.spec.ts
@@ -0,0 +1,16 @@
+import { getEl, getText, isBuild } from '../../testUtils';
+
+describe('inspector-vite', () => {
+	it('should render page', async () => {
+		expect(await getText('h1')).toBe('Hello Inspector!');
+	});
+	if (!isBuild) {
+		it('should show inspector toggle during dev', async () => {
+			expect(await getEl('.svelte-inspector-toggle')).not.toBe(null);
+		});
+	} else {
+		it('should not show inspector toggle during preview', async () => {
+			expect(await getEl('.svelte-inspector-toggle')).toBe(null);
+		});
+	}
+});
