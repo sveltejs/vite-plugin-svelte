85ec95602b3871e6f6cc91b33e142e4a5bacc9c8
dominikg <dominik.goepel@gmx.de>
2022-08-30 15:29:09 +0200

    fix: load inspector files with a clean filename; update lock and types

---
+++ .changeset/short-clocks-flow.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/vite-plugin-svelte': patch
+---
+
+fix svelte-inspector import for vite 3.1
---
+++ packages/vite-plugin-svelte/src/ui/inspector/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Prefix for resolved fs paths, since windows paths may not be valid as URLs.
+ */
+const FS_PREFIX = `/@fs/`;
+const IS_WINDOWS = process.platform === 'win32';
+const queryRE = /\?.*$/s;
+const hashRE = /#.*$/s;
+
+export function idToFile(id: string): string {
+	// strip /@fs/ but keep leading / on non-windows
+	if (id.startsWith(FS_PREFIX)) {
+		id = id = id.slice(IS_WINDOWS ? FS_PREFIX.length : FS_PREFIX.length - 1);
+	}
+	// strip query and hash
+	return id.replace(hashRE, '').replace(queryRE, '');
+}
