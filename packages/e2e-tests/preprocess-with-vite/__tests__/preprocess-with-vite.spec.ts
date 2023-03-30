5e9ec7023c465e023f227e1ef01c76b74ce9f3ee
Dominik G <dominik.goepel@gmx.de>
2023-03-30 09:06:58 +0200

    fix(vitePreprocess): remove dangling pure annotations (#609)

--- packages/e2e-tests/preprocess-with-vite/__tests__/preprocess-with-vite.spec.ts
+++ packages/e2e-tests/preprocess-with-vite/__tests__/preprocess-with-vite.spec.ts
@@ -1,4 +1,5 @@
-import { getColor, getText } from '~utils';
+import { getColor, getText, browserLogs } from '~utils';
+import { expect } from 'vitest';
 
 test('should render App', async () => {
 	expect(await getText('h1.foo')).toBe(`Hello world`);
@@ -6,4 +7,9 @@ test('should render App', async () => {
 	expect(await getText('#foo-title')).toBe('Styles with stylus blub');
 	expect(await getColor('#foo-title')).toBe('magenta');
 	expect(await getColor('p.note')).toBe('rgb(255, 62, 0)');
+});
+
+test('should not mangle code from esbuild pure annotations', async () => {
+	expect(browserLogs.some((log) => log.startsWith('pure test 1'))).toBe(true);
+	expect(browserLogs.some((log) => log.startsWith('pure test 2'))).toBe(true);
 });
