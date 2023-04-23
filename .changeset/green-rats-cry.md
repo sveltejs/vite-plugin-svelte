46aeedf6ba007b4d7fa02f3ccdbfbeef01ead9a4
dominikg <dominik.goepel@gmx.de>
2023-04-23 17:30:33 +0200

    fix(inspector): automatically disable on leave after a file has been opened

---
+++ .changeset/green-rats-cry.md
@@ -0,0 +1,5 @@
+---
+'@sveltejs/vite-plugin-svelte': patch
+---
+
+fix(inspector): after a file has been opened, automatically disable inspector on leaving browser
--- packages/vite-plugin-svelte/src/ui/inspector/Inspector.svelte
+++ packages/vite-plugin-svelte/src/ui/inspector/Inspector.svelte
@@ -6,6 +6,7 @@
 	const toggle_combo = options.toggleKeyCombo?.toLowerCase().split('-');
 	const nav_keys = Object.values(options.navKeys).map((k) => k.toLowerCase());
 	let enabled = false;
+	let hasOpened = false;
 
 	const icon = `data:image/svg+xml;base64,${btoa(
 		`
@@ -124,6 +125,7 @@
 		if (file_loc) {
 			stop(event);
 			fetch(`/__open-in-editor?file=${encodeURIComponent(file_loc)}`);
+			hasOpened = true;
 			if (options.holdMode && is_holding()) {
 				disable();
 			}
@@ -247,6 +249,7 @@
 
 	function disable() {
 		enabled = false;
+		hasOpened = false;
 		enabled_ts = null;
 		const b = document.body;
 		listeners(b, enabled);
@@ -255,6 +258,18 @@
 			active_el?.classList.remove('svelte-inspector-active-target');
 		}
 		active_el = null;
+	}
+
+	function visibilityChange() {
+		if (document.visibilityState === 'hidden') {
+			onLeave();
+		}
+	}
+
+	function onLeave() {
+		if (hasOpened) {
+			disable();
+		}
 	}
 
 	onMount(() => {
@@ -269,6 +284,8 @@
 				document.body.addEventListener('keyup', keyup);
 			}
 		}
+		document.addEventListener('visibilitychange', visibilityChange);
+		document.documentElement.addEventListener('mouseleave', onLeave);
 		return () => {
 			// make sure we get rid of everything
 			disable();
@@ -282,6 +299,8 @@
 					document.body.addEventListener('keyup', keyup);
 				}
 			}
+			document.removeEventListener('visibilitychange', visibilityChange);
+			document.documentElement.removeEventListener('mouseleave', onLeave);
 		};
 	});
 </script>
