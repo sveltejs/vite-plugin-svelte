3fd8675e943e76103b46b386acc5862477205561
dominikg <dominik.goepel@gmx.de>
2024-03-29 22:22:47 +0100

    fix: generate list of svelte exports to optimize and dedupe so it works for svelte4 and 5

--- packages/vite-plugin-svelte/src/utils/constants.js
+++ packages/vite-plugin-svelte/src/utils/constants.js
@@ -1,31 +1,30 @@
 import { isSvelte5 } from './svelte-version.js';
+import { createRequire } from 'node:module';
 
 export const VITE_RESOLVE_MAIN_FIELDS = ['browser', 'module', 'jsnext:main', 'jsnext'];
 
 export const SVELTE_RESOLVE_MAIN_FIELDS = ['svelte'];
 
-export const SVELTE_IMPORTS = [
-	'svelte/animate',
-	'svelte/easing',
-	'svelte/internal',
-	'svelte/internal/disclose-version',
-	'svelte/motion',
-	'svelte/ssr',
-	'svelte/store',
-	'svelte/transition',
-	'svelte'
-];
+export const SVELTE_IMPORTS = Object.entries(
+	createRequire(import.meta.url)('svelte/package.json').exports
+)
+	.map(([name, config]) => {
+		// ignore type only
+		if (typeof config === 'object' && Object.keys(config).length === 1 && config.types) {
+			return '';
+		}
+		// ignore names
+		if (name === './package.json' || name === './compiler') {
+			return '';
+		}
+		return name.replace(/^\./, 'svelte');
+	})
+	.filter((s) => s.length > 0);
 
-export const SVELTE_HMR_IMPORTS = [
-	'svelte-hmr/runtime/hot-api-esm.js',
-	'svelte-hmr/runtime/proxy-adapter-dom.js',
-	'svelte-hmr'
-];
-
-if (isSvelte5) {
-	SVELTE_IMPORTS.push('svelte/server', 'svelte/internal/server', 'svelte/legacy');
-	SVELTE_HMR_IMPORTS.length = 0; // truncate, svelte-hmr isn't used with svelte5
-}
+// svelte-hmr isn't used with svelte5
+export const SVELTE_HMR_IMPORTS = isSvelte5
+	? []
+	: ['svelte-hmr/runtime/hot-api-esm.js', 'svelte-hmr/runtime/proxy-adapter-dom.js', 'svelte-hmr'];
 
 export const SVELTE_EXPORT_CONDITIONS = ['svelte'];
 
