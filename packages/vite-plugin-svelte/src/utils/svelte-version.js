acdf0da6794de0b9e0ec7564e540b64e6b53258c
Dominic Gannaway <dg@domgan.com>
2024-04-10 19:44:31 +0100

    link to version 96

--- packages/vite-plugin-svelte/src/utils/svelte-version.js
+++ packages/vite-plugin-svelte/src/utils/svelte-version.js
@@ -14,4 +14,4 @@ export const isSvelte5 = VERSION.startsWith('5.');
  * @type {boolean}
  */
 export const isSvelte5WithHMRSupport =
-	VERSION.startsWith('5.0.0-next.') && Number(VERSION.slice(11)) > 97;
+	VERSION.startsWith('5.0.0-next.') && Number(VERSION.slice(11)) > 96;
