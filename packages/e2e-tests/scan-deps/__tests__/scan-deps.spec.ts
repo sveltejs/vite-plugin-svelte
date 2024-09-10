139710c1975da0da9ce9cd973ce610debea905a1
paoloricciuti <ricciutipaolo@gmail.com>
2024-09-10 22:26:03 +0200

    fix: actual test

--- packages/e2e-tests/scan-deps/__tests__/scan-deps.spec.ts
+++ packages/e2e-tests/scan-deps/__tests__/scan-deps.spec.ts
@@ -1,8 +1,9 @@
-import { e2eServer, getText, isBuild, serverLogs } from '~utils';
+import { e2eServer } from '~utils';
 
-test('should discover dependencies exported from script module', async () => {
-	//expect(e2eServer.logs.server.out.length).toBeGreaterThan(1);
-	console.log('server logs', e2eServer.logs.server.out);
-	expect(await getText('p')).toBe('42');
-	//expect(serverLogs.some((log) => log.includes('No matching export in'))).toBeFalsy();
+test('should not fail to discover dependencies exported from script module', async () => {
+	expect(
+		e2eServer.logs.server.err.some((logs) =>
+			logs.match(/No matching export in "html:.+\/src\/Deps\.svelte" for import "something"/)
+		)
+	).toBe(false);
 });
