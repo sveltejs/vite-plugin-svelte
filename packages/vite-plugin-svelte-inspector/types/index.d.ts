94ab3505c7744568f76e12d480cc21214f0eeb12
bluwy <bjornlu.dev@gmail.com>
2024-11-25 15:54:45 +0800

    chore: update types

--- packages/vite-plugin-svelte-inspector/types/index.d.ts
+++ packages/vite-plugin-svelte-inspector/types/index.d.ts
@@ -74,7 +74,7 @@ declare module '@sveltejs/vite-plugin-svelte-inspector' {
 			base: string;
 		};
 	}
-	export function svelteInspector(options?: Partial<Options> | undefined): import("vite").Plugin;
+	export function svelteInspector(options?: Partial<Options>): import("vite").Plugin;
 
 	export {};
 }
