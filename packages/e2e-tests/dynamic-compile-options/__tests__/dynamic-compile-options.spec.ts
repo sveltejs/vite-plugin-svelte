6fa927e24ee3c068efe87347fecdf075905a1352
dominikg <dominik.goepel@gmx.de>
2024-12-15 11:53:52 +0100

    do'h

--- packages/e2e-tests/dynamic-compile-options/__tests__/dynamic-compile-options.spec.ts
+++ packages/e2e-tests/dynamic-compile-options/__tests__/dynamic-compile-options.spec.ts
@@ -1,6 +1,6 @@
 import { getText } from '~utils';
 
-test('should respect dynamic compile option preserveWhitespace: true for A', async () => {
+test('should respect dynamic compile option preserveWhitespace: true', async () => {
 	expect(await getText('#A')).toBe('    preserved leading whitespace');
 	expect(await getText('#B')).toBe('removed leading whitespace');
 	expect(await getText('#C')).toBe('    preserved leading whitespace');
