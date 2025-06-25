dfbc8e450da81129b8e02f4df286ab30cd26d440
dominikg <dominik.goepel@gmx.de>
2025-06-25 15:34:10 +0200

    chore: add test for preprocessor transform order

---
+++ packages/e2e-tests/preprocess-with-vite/src/Preprocess.svelte
@@ -0,0 +1,4 @@
+<h2>order of preprocess transforms</h2>
+<ol id="transforms-list">
+	<!-- various transforms from vite config inject list items here-->
+</ol>
--- packages/e2e-tests/preprocess-with-vite/vite.config.js
+++ packages/e2e-tests/preprocess-with-vite/vite.config.js
@@ -1,10 +1,109 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import MagicString from 'magic-string';
+
+function addListItemTransform(code, filename, value) {
+	const index = code.indexOf('</ol>');
+	if (index < 0) {
+		return;
+	}
+	const s = new MagicString(code);
+	s.prependLeft(index, `<li>${value}</li>\n`);
+	return {
+		code: s.toString(),
+		map: s.generateDecodedMap({ hires: 'boundary', file: filename, includeContent: false })
+	};
+}
+
+function beforePreprocess() {
+	return {
+		name: 'before-preprocess',
+		enforce: 'pre',
+		transform: {
+			order: 'pre',
+			filter: { id: { include: /Preprocess\.svelte/, exclude: /lang\.css$/ } },
+			async handler(code, id) {
+				return addListItemTransform(
+					code,
+					id,
+					'before svelte preprocessors: vite-plugin (enforce: pre) transform (order: pre)'
+				);
+			}
+		}
+	};
+}
+function afterPreprocess() {
+	return {
+		name: 'after-preprocess',
+		transform: {
+			filter: { id: { include: /Preprocess\.svelte/, exclude: /lang\.css$/ } },
+			async handler(code, id) {
+				return addListItemTransform(
+					code,
+					id,
+					'after svelte preprocessors: vite-plugin (default) transform (default)'
+				);
+			}
+		}
+	};
+}
+
+function afterCompile() {
+	return {
+		name: 'after-svelte',
+		transform: {
+			order: 'post',
+			filter: { id: { include: /Preprocess\.svelte/, exclude: /lang\.css$/ } },
+			async handler(code, id) {
+				return addListItemTransform(
+					code,
+					id,
+					'after svelte compile: vite-plugin (default) transform (order: post)'
+				);
+			}
+		}
+	};
+}
+
+function apiPreprocessor() {
+	return {
+		name: 'api-preprocess',
+		enforce: 'pre',
+		api: {
+			sveltePreprocess: {
+				markup: ({ content, filename }) =>
+					addListItemTransform(
+						content,
+						filename,
+						'vite-plugin-svelte:preprocess: preprocessor from vitePlugin.api.sveltePreproess'
+					)
+			}
+		}
+	};
+}
 
 export default defineConfig(({ command, mode }) => {
 	const isProduction = mode === 'production';
 	return {
-		plugins: [svelte()],
+		plugins: [
+			beforePreprocess(),
+			apiPreprocessor(),
+			svelte({
+				preprocess: [
+					vitePreprocess({ script: true }),
+					{
+						markup: ({ content, filename }) =>
+							addListItemTransform(
+								content,
+								filename,
+								'vite-plugin-svelte:preprocess: preprocessor from config'
+							)
+					}
+				]
+			}),
+			afterPreprocess(),
+			afterCompile()
+		],
 		build: {
 			minify: isProduction
 		}
