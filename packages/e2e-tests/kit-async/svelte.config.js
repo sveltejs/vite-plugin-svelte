2e4f910de52b21e1ae7987128ac9696c1c072bd7
dominikg <dominik.goepel@gmx.de>
2025-07-15 19:01:29 +0200

    bump svelte&kit, update kit-async setup

--- packages/e2e-tests/kit-async/svelte.config.js
+++ packages/e2e-tests/kit-async/svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: node()
 	},
 	compilerOptions: {
 		experimental: {
