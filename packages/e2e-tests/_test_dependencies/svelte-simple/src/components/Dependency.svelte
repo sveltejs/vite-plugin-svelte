bb1e56c42bd26c1df0476908dda9312251050183
dominikg <dominik.goepel@gmx.de>
2025-12-03 22:27:26 +0100

    work around sourcemap bug in vite-8 beta

--- packages/e2e-tests/_test_dependencies/svelte-simple/src/components/Dependency.svelte
+++ packages/e2e-tests/_test_dependencies/svelte-simple/src/components/Dependency.svelte
@@ -1,7 +1,9 @@
 <script>
-	const label = 'dependency-import';
 	import * as cjsOnly from 'e2e-test-dep-cjs-only';
+	const label = 'dependency-import';
 	const { cjs } = cjsOnly;
+	// moving the following script close tag to column 0 triggers a bug in svelte sourcemap creation that prevents rolldown optimizer from processing the sourcemap
+	// TODO: move it back after fixed in svelte see https://github.com/sveltejs/svelte/pull/17167
 </script>
 
 <div id="dependency-import"><span class="label">{label}</span></div>
